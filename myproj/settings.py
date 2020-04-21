73c49a2747cb5abb63fc2ab79f2758cb36fd7247
mohammed-aws1 <64014758+mohammed-aws1@users.noreply.github.com>
2020-04-21 17:22:19 +0300

    Update settings.py

--- myproj/settings.py
+++ myproj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5kl#4k^2aae*y%!j@apf$19wcc8_^mf_=jf60imb6ji!+rqg47'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["aws-aws.herokuapp.com"]
 EMAIL_HOST ='smtp.gmail.com'
