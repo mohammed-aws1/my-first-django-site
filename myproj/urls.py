571c7d5bbb13d675d3c209b40ea8f168a1b37fe5
mohammed-aws1 <64014758+mohammed-aws1@users.noreply.github.com>
2020-04-21 22:47:09 +0300

    Update urls.py

--- myproj/urls.py
+++ myproj/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
-if settings.DEBUG :
+if not settings.DEBUG :
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
